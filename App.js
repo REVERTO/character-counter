9f845541cb3f714c1e15a9adcf9af4e7db572410
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-09 00:46:34 +0900

    feat: impl character counter

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: 'テキストを入力、またはコピーしてください',
+      text: '',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          style={styles.textinput}
+          onChangeText={(text) => this.setState({text})}
+          placeholder={this.state.placeholder}
+          value={this.state.text}
+          multiline={true}
+        />
+        <Text>{this.textCount()}</Text>
       </View>
     );
   }
+
+  textCount() {
+    return `文字数: ${this.state.text.length}`
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +38,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textinput: {
+    width: 300,
+    height: 100,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
